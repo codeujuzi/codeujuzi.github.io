328704f6cdae8e6f94d91f5949c937f6f7f4acea
Githaiga22 <allangithaiga5@gmail.com>
2024-09-19 01:37:35 +0300

    add files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const Groq = require('groq-sdk');
+import Groq from 'groq-sdk';
+
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 // const groq = new Groq();
@@ -23,4 +24,9 @@ async function main() {
   }
 }
 
-main();
+function translatePage(){
+  const content = document.getElementById('description')
+  console.log(content.textContent)
+}
+
+export default { translatePage };
